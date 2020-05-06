1415f91b5f47ee2092ca9d615bcad64bc3cba00a
baa <34665120+baa-mb@users.noreply.github.com>
2020-05-07 00:23:00 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
-input.onButtonPressed(Button.AB, function () {
-    dauerlauf = 1
-})
 input.onButtonPressed(Button.A, function () {
     dauerlauf = 1
     Helligkeit(3)
 })
+input.onButtonPressed(Button.AB, function () {
+    dauerlauf = 1
+})
+function Helligkeit (hell: number) {
+    range1.setBrightness(hell)
+    range2.setBrightness(hell)
+    range3.setBrightness(hell)
+}
 input.onButtonPressed(Button.B, function () {
     strip.clear()
     strip.show()
@@ -12,16 +17,12 @@ input.onButtonPressed(Button.B, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
 })
-function Helligkeit(hell: number) {
-    range1.setBrightness(hell)
-    range2.setBrightness(hell)
-    range3.setBrightness(hell)
-}
 let range3: neopixel.Strip = null
 let range2: neopixel.Strip = null
 let range1: neopixel.Strip = null
 let dauerlauf = 0
 let strip: neopixel.Strip = null
+let zeit = 200
 strip = neopixel.create(DigitalPin.P0, 8, NeoPixelMode.RGB)
 dauerlauf = 1
 range1 = strip.range(0, 3)
@@ -30,24 +31,24 @@ range3 = strip.range(3, 2)
 basic.forever(function () {
     if (dauerlauf) {
         range1.showColor(neopixel.colors(NeoPixelColors.Purple))
-        basic.pause(500)
+        basic.pause(zeit)
         range1.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.pause(500)
+        basic.pause(zeit)
         range1.showColor(neopixel.colors(NeoPixelColors.Blue))
-        basic.pause(500)
+        basic.pause(zeit)
         range2.showColor(neopixel.colors(NeoPixelColors.Green))
-        basic.pause(500)
+        basic.pause(zeit)
         range2.showColor(neopixel.colors(NeoPixelColors.White))
-        basic.pause(500)
+        basic.pause(zeit)
         range2.showColor(neopixel.colors(NeoPixelColors.Red))
-        basic.pause(500)
+        basic.pause(zeit)
         range3.showColor(neopixel.colors(NeoPixelColors.Indigo))
-        basic.pause(500)
+        basic.pause(zeit)
         range3.showColor(neopixel.colors(NeoPixelColors.Violet))
-        basic.pause(500)
+        basic.pause(zeit)
     } else {
         strip.rotate(1)
         strip.show()
-        basic.pause(200)
+        basic.pause(zeit)
     }
 })
